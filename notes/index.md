93952a1de37b26fee4f83e7c8ac34a3f2daa61fd
pradip <chaudharypradip678@gmail.com>
2025-02-01 16:44:44 +0545

    init

--- notes/index.md
+++
@@ -1,9 +0,0 @@
-# Next.js 15 Learning Notes
-
-## Table of Contents
-
-1. [Introduction](01-introduction.md)
-2. [Routing](02-routing.md)
-3. [Styling](03-styling.md)
-4. [API Routes](04-api.md)
-5. [Deployment](05-deployment.md)
--- routing/src/app/service/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-
-    const id = (await params).id
-    return (
-        <div>
-            <h1>Service : {id}</h1>
-        </div>
-    )
-}
